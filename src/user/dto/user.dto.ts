f7f9cbba580fb4e9595d0c8f1db61b9e1fa203d0
Sthepen_EA <sthepen.espinoza29@gmail.com>
2024-11-20 00:26:49 -0500

    nest validations added to dtos

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,12 +1,19 @@
+import { IsEmail, IsIn, IsNotEmpty } from 'class-validator';
 import { Role } from '../user.entity';
 
 export class UserDto {
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
 
 export class UpdateUserDto {
   username?: string;
   email?: string;
+
+  @IsIn(Object.values(Role))
   role?: Role;
 }
